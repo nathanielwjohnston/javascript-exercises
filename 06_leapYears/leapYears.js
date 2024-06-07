abf1905b8849070fc97e63d62446b377f7be6ad5
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 20:11:49 +0100

    Add missing conditional

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
-            if (year % 400) {
+            if (year % 400 === 0) {
                 return true;
             }
             return false;
@@ -9,6 +9,7 @@ const leapYears = function(year) {
         return true;
     }
 
+    return false;
 };
 
 // Do not edit below this line
