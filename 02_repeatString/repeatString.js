ca08effb48cc5d0edd4330ea6d72befd939d6afd
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 14:29:10 +0100

    Add loop to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    repeatedString = "";
+    for (i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
