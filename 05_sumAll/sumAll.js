6d00963d582b9258e4abdc5ce161d42c9fe9205c
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 20:01:03 +0100

    Throw error in certain cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || start < 0 || !Number.isInteger(end) || end < 0) {
+        return "ERROR"
+    }
     sum = null;
     for (let i = start; i <= end; i++) {
         sum += i;
