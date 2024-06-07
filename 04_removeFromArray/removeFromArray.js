a14bffe222f87b579a5921f5f61d31a67328c319
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 15:24:13 +0100

    Add functionality to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valueToRemove) {
+    index = array.indexOf(valueToRemove);
+    array.splice(index, 1);
+
+    return array;
 };
 
 // Do not edit below this line
