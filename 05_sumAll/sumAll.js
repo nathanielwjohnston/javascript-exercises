69dcebf60e46adaa4a8fb0e618a8c212b5031a47
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 19:50:51 +0100

    Add functionality to sum two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    sum = null;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
