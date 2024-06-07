aab4b49b49a45027db4dc5d08166264cd0711385
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 20:25:07 +0100

    Add ability to round to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temp) {
-  return (temp - 32) * (5/9);
+  return parseFloat(((temp - 32) * (5/9)).toFixed(1));
 };
 
 const convertToFahrenheit = function(temp) {
-  return temp * (9/5) + 32
+  return parseFloat((temp * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
