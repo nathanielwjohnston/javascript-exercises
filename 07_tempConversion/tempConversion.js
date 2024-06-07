c879335df8fbc9c92521fa4a32d7513ce6437e66
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-07 20:20:32 +0100

    Add basic celsius to fahrenheit conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,8 @@ const convertToCelsius = function(temp) {
   return (temp - 32) * (5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return temp * (9/5) + 32
 };
 
 // Do not edit below this line
