a808cde58f655aefccedc5489aa241f687443a1d
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-17 22:02:38 +0100

    Make function work in basic cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    a = 1;
+    b = 1;
+    for (i = 1; i < num; i++) {
+        current = a + b;
+        a = b;
+        b = current
+    }
 
+    return a;
 };
 
 // Do not edit below this line
