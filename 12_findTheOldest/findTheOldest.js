09a5e0b7c8e868dd4a1026e96d9afb77af6369d0
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-17 23:13:14 +0100

    Account for people who have no death date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,20 @@
 const findTheOldest = function(people) {
     people.sort((person1, person2) => {
-        return (person2.yearOfDeath - person2.yearOfBirth) - 
-            (person1.yearOfDeath - person1.yearOfBirth)
+        let person1Age;
+        let person2Age;
+        if (!person1.yearOfDeath) {
+            person1Age = (new Date()).getFullYear() - person1.yearOfBirth;
+        } else {
+            person1Age = person1.yearOfDeath - person1.yearOfBirth;
+        }
+
+        if (!person2.yearOfDeath) {
+            person2Age = (new Date()).getFullYear() - person2.yearOfBirth;
+        } else {
+            person2Age = person2.yearOfDeath - person2.yearOfBirth;
+        }
+        return (person2Age) - 
+            (person1Age);
             
     })
 
