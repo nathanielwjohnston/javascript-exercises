8aa864e81016825a9f842ddfb5533041a7da592c
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-17 22:21:23 +0100

    Find the oldest person using the sort method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((person1, person2) => {
+        return (person2.yearOfDeath - person2.yearOfBirth) - 
+            (person1.yearOfDeath - person1.yearOfBirth)
+            
+    })
 
+    return people[0];
 };
 
 // Do not edit below this line
